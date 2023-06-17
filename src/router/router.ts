f5d0bc7d5d86e03039677e46c9540b3aa0032c91
[bruno rodrigues] <brunoramosro@gmail.com>
2023-06-17 11:17:56 -0400

    aula1

---
+++ src/router/router.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import mainController from '../controllers/main';
+import departamentoController from '../controllers/departamento';
+
+const router = Router();
+
+// Main controller
+router.get('/', mainController.index);
+router.get('/about', mainController.about);
+router.get('/ui', mainController.ui);
+
+// Departamento controller
+router.get('/departamento', departamentoController.index);
+router.get('/departamento/create', departamentoController.create);
+router.post('/departamento/create', departamentoController.create);
+router.get('/departamento/update/:id', departamentoController.update);
+router.post('/departamento/update/:id', departamentoController.update);
+router.get('/departamento/:id', departamentoController.read);
+router.post('/departamento/:id', departamentoController.remove);
+
+export default router;
